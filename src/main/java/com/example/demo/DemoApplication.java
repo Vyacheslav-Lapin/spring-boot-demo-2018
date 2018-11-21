efbfb15ad0d992af8ab054c7fcdfaa472c78c43d
Vyacheslav Lapin <Vyacheslav.Lapin@gmail.com>
2018-11-21 15:26:43 +0300

    resource example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,47 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static lombok.AccessLevel.PRIVATE;
 
 @SpringBootApplication
 public class DemoApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
+  }
+}
+
+@RepositoryRestResource
+interface CatRepository extends JpaRepository<Cat, Long> {
+}
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = PRIVATE)
+class Cat {
+
+  @Id
+  @GeneratedValue
+  Long id;
+
+  String name;
+
+  public Cat(String name) {
+    this.name = name;
   }
 }
